030b4cd7a9e627143bc204730a9ee1f3300e2472
jkraasch <33602014+jkraasch@users.noreply.github.com>
2018-08-03 12:08:59 -0700

    Add files via upload

---
+++ VommJava/vomm/lib/vomm/VMM.java
@@ -0,0 +1,473 @@
+package vomm;
+
+import com.cycling74.max.Atom;
+import lib.*;
+import org.apache.commons.math3.distribution.*;
+import java.io.*;
+import java.util.*;
+
+
+public class VMM implements java.io.Serializable{
+
+
+    // Non-Unique list of masks
+    private ArrayList<ArrayList<Integer>> permutations = new ArrayList<ArrayList<Integer>>();
+    // Transfer-Matrix of VOMM
+    private ArrayList<Pandas> prob_mats;
+    // Counts of Symbol given Contexts
+    private ArrayList<Pandas> counts;
+    //Initial alphabet may not be correct if new symbols are added
+    private ArrayList<String> alphabet;
+    //String[]alphabet transformed to their index in VOMM
+    private int[] alpha_pos;
+    // Maximal order of VOMM
+    private int max_depth;
+    //Unique Masks used for fuzzy sampling (filter to acquire contexts that are similar to the input)
+    private Set<ArrayList<Integer>> masks = new HashSet<ArrayList<Integer>>();
+    //history of samples
+    private ArrayList<String> generated_history = new ArrayList<String>();
+    //history of seeds
+    private ArrayList input_history = new ArrayList<String>();
+    //Typicality used for modulating the probability
+    public double typicality = 1.0;
+
+    /**
+     * Constructor Initializes parameters need for VOMM
+     * @param alphabet ArrayList<String> of strings used in the dataset
+     * @param max_depth int maximal order of layers in VOMM
+     */
+    public VMM(ArrayList<String> alphabet, int max_depth){
+        this.alphabet = alphabet;
+        //this.vmm = new ArrayList<Pandas>();
+        this.counts = new ArrayList<Pandas>();
+        this.alpha_pos = new int[alphabet.size()];
+        this.max_depth = max_depth;
+        for (int i = 0; i < alphabet.size(); i++){
+            this.alpha_pos[i] = i;
+        }
+    }
+
+    /**
+     *
+     * Generates VOMM from Dataset of sequences
+     * @param sequence Dataset of sequences to learn from
+     */
+    public void learn(ArrayList<String> sequence){
+        int level = 0;
+        while((level <= this.max_depth)) {
+            Pandas df = new Pandas(this.alphabet, level);
+            this.counts.add(df);
+            if (sequence.size()-level >= 0 ){
+                this.fillPandas(sequence, level);
+            }
+            level++;
+        }
+        this.prob_mats = this.copy(this.counts);
+        this.compute_prob_mat();
+    }
+
+
+    public void update_generated_history(ArrayList<String> generated){
+
+        generated_history.addAll(generated);
+        if(generated_history.size()> this.max_depth){
+            this.generated_history = new ArrayList<String>(this.generated_history.subList(generated_history.size()-max_depth, this.generated_history.size()));
+        }
+
+    }
+
+    public void update_input_history(ArrayList<String> input){
+
+        input_history.addAll(input);
+        if(input_history.size()> this.max_depth){
+            this.input_history = new ArrayList<String>(this.input_history.subList(input_history.size()-max_depth,input_history.size()));
+        }
+    }
+
+    /**
+     * updates VOMM according to a history and the symbol appearing afterwards
+     *
+     * @param history String of chars appearing before symbol
+     * @param symbol String that appears after history
+     */
+    public void update(ArrayList<String> history, String symbol){
+        int depth = history.size() < this.max_depth? history.size(): this.max_depth;
+        if(depth == 0){
+            this.counts.get(0).incrementValue(history, symbol);
+
+            ArrayList<Double> values = (ArrayList<Double>) this.counts.get(0).getValue(history).clone();
+            int dividend = (int)Helper.sum_array(values);
+            this.prob_mats.get(0).setValue(history, Helper.divide_array(values, dividend));
+        }
+        for (int order = 0; order < depth; order++){
+            ArrayList<String> context = new ArrayList<String>(history.subList(history.size()-order, history.size()));
+            System.out.println("in");
+            this.counts.get(order).incrementValue(context, symbol);
+            int dividend= (int)Helper.sum_array(this.counts.get(order).getValue(context));
+            this.prob_mats.get(order).setValue(context, Helper.divide_array(this.counts.get(order).getValue(context), dividend));
+        }
+    }
+    /**
+     * Clears stored history of last samples (generate_history)
+     */
+    public void clearGeneratedHistory(){
+        this.generated_history = new ArrayList<String>();
+    }
+    public void clearInputHistory(){
+        new ArrayList<String>();
+    }
+    /**
+     * Clears input_history and generated_history
+     */
+    public void clearWholeHistory(){
+        this.input_history = new ArrayList<String>();
+        this.generated_history = new ArrayList<String>();
+    }
+
+    /**
+     * Returns probability of Symbol appearing after context
+     * @param context String that appeared before symbol
+     * @param symbol String that you want to no the probability of appearing from
+     * @return probability P(symbol|context)
+     */
+    public double predict(ArrayList<String> context, String symbol){
+        if (this.prob_mats.get(context.size()).getValue(context, symbol) == 0){return this.predict(new ArrayList<String>(context.subList(1, context.size())),symbol);}
+        return this.prob_mats.get(context.size()).getValue(context, symbol);
+    }
+
+    /**
+     * Samples Symbol from probabilities of Symbols appearing after empty sequence
+     * @param typicality value between 1-0 to decide the typicality of the probabilities
+     * @return Symbol sampled from the probability distribution following an empty seed
+     */
+    public Atom[] sample(double typicality){
+        ArrayList<Double> probabilities = this.prob_mats.get(0).getValue("");
+        probabilities = Helper.modulate(probabilities, typicality);
+        Double[] Double_array = new Double[probabilities.size()];
+        Double_array = probabilities.toArray(Double_array);
+        int i = 0;
+        double[] array_probs = new double[Double_array.length];
+        for(Double d : Double_array) {
+            array_probs[i] = (double)d;
+            i++;
+        }
+        EnumeratedIntegerDistribution dist = new EnumeratedIntegerDistribution(this.prob_mats.get(0).alpha_pos, array_probs);
+        int idx = dist.sample();
+        String sample = this.alphabet.get(idx);
+        update_generated_history(sample);
+        Atom[] dumpAtom = new Atom[]{Atom.newAtom(sample),Atom.newAtom(array_probs[idx])};
+        return dumpAtom;
+    }
+
+    /**
+     * Returns one character sampling from probabilities of Symbols appearing after context
+     *
+     * @param seed Seed used to sample next symbol
+     * @param typicality value between 1-0 to decide the typicality of the probabilities
+     * @param max_order int Maximal order of VOMM that should be used
+     * @return Sampled Symbol
+     */
+
+    public Atom[] sample(String seed, double typicality, int max_order){
+        if (max_order > this.prob_mats.size()){throw new RuntimeException("Context too long");}
+        if(max_order < 0){throw new RuntimeException("Negative order impossible");}
+        if(seed.length() > max_order){seed = seed.substring(seed.length()-max_order);}
+
+        ArrayList<Double> probabilities = this.prob_mats.get(seed.length()).getValue(seed);
+        probabilities = Helper.modulate(probabilities, typicality);
+        double sum = Helper.sum_array(probabilities);
+
+        //If the context did not appear reduce order by 1
+        if (sum != 1){
+            System.out.println(sum); return this.sample(seed.substring(1),typicality, max_order);}
+        //Sample-method from apache commons
+        Double[] Double_array = new Double[probabilities.size()];
+        Double_array = probabilities.toArray(Double_array);
+        int i = 0;
+        double[] array_probs = new double[Double_array.length];
+        for(Double d : Double_array) {
+            array_probs[i] = (double)d;
+            i++;
+        }
+        EnumeratedIntegerDistribution dist = new EnumeratedIntegerDistribution(this.prob_mats.get(0).alpha_pos, array_probs);
+        int idx = dist.sample();
+        String sample = this.alphabet.get(idx);
+        update_generated_history(sample);
+        Atom[] dumpAtom = new Atom[]{Atom.newAtom(sample),Atom.newAtom(array_probs[idx])};
+        return dumpAtom;
+    }
+
+    /**
+     * Sampling Symbol from the mean probability distribution described by similar contexts to input(Similarity-Measure = Hamming-Distance)
+     * @param seed String context to get the similar contexts of     *
+     * @param distance Sets the amount of Hamming-distance
+     * @param typicality value between 1-0 to decide the typicality of the probabilities
+     * @return Sampled symbol
+     */
+    public Atom[] sample_fuzzy(String seed, int distance, double typicality, int max_order){
+        // Getting rid of unwanted cases
+        if (max_order > this.prob_mats.size())throw new RuntimeException("Context too long");
+        if(max_order < 0)throw new RuntimeException("Negative order impossible");
+        if(distance > seed.length() || distance < 0)throw new RuntimeException("Illegal distance ");
+        if(seed.length() > max_order)seed = seed.substring(seed.length()-max_order);
+
+        /*
+        If needed masks not yet generated generate masks
+        these masks are ints corresponding to boolean arrays that check if only n-values in the contexts are different
+        from the original context (n = distance)
+        */
+        if (this.masks.size() != seed.length()) this.generate_masks(seed.length(), distance);
+
+        Pandas df = this.prob_mats.get(seed.length());
+        ArrayList<Integer> idx;
+        List<byte[]> contexts = new ArrayList<byte[]>(this.prob_mats.get(seed.length()).binarized.values());
+        byte[] is_similar_to = this.prob_mats.get(seed.length()).binarized.get(seed);
+
+        //If context not appeared escape to order -1
+        if(is_similar_to == null){return sample(seed.substring(1),typicality, max_order);}
+
+        //Get Contexts similar to input context by filtering with masks (ln.170) and get mean-probability
+        idx = get_similar(is_similar_to, contexts );
+        ArrayList<Double> sum = new ArrayList<Double>(Collections.nCopies(df.alphabet.size(), 0.0));
+        for(int id: idx){
+            ArrayList<Double> probabilities = df.getValue(df.inverse_index_0.get(id));
+            probabilities = Helper.modulate(probabilities, typicality);
+            for(int i = 0; i < probabilities.size(); i++){
+                Double value = sum.get(i);
+                sum.set(i, value + probabilities.get(i));
+            }
+        }
+        //getting mean probability
+        ArrayList<Double> mean = Helper.divide_array(sum, idx.size());
+
+        //escape if probability does not sum up to 1
+        if (Helper.sum_array(mean) != 1){return this.sample(seed.substring(1),typicality, max_order);}
+
+        //sampling from distribution
+        Double[] Double_array = new Double[mean.size()];
+        Double_array = mean.toArray(Double_array);
+        int i = 0;
+        double[] array_probs = new double[Double_array.length];
+        for(Double d : Double_array) {
+            array_probs[i] = (double)d;
+            i++;
+        }
+        EnumeratedIntegerDistribution dist = new EnumeratedIntegerDistribution(this.prob_mats.get(0).alpha_pos, array_probs);
+        int id = dist.sample();
+        String sample = this.alphabet.get(id);
+        update_generated_history(sample);
+        Atom[] dumpAtom;
+        dumpAtom = new Atom[]{Atom.newAtom(sample),Atom.newAtom(array_probs[id])};
+        return dumpAtom;
+    }
+
+
+
+    /**
+     * Updates DataFrame representing VOMM at order depth
+     * @param seq Data to learn from
+     * @param depth VOMM order to update
+     */
+    private void fillPandas(ArrayList<String> seq, int depth){
+        int extra_depth = depth +1;
+        for (int i = 0; i <= seq.size() - extra_depth; i++) {
+            ArrayList<String> context = new ArrayList<String>(seq.subList(i, i + (depth)));
+            String symbol = seq.get(i + depth));
+            this.counts.get(depth).incrementValue(context, symbol);
+        }
+        this.alphabet = this.counts.get(depth).alphabet;
+    }
+
+    /**
+     * Computes Probability Matrix used for sampling
+     */
+    private void compute_prob_mat(){
+        for(Pandas df: this.prob_mats){
+            for(int i = 0; i < df.length(); i++){
+                ArrayList<String> context = df.getContext(i);
+                ArrayList<Double> values = df.getValue(context);
+                double sum = Helper.sum_array(values);
+                ArrayList<Double> probability = Helper.divide_array(values, sum);
+                df.setValue(context, probability);
+
+            }
+        }
+    }
+
+    /**
+     * Generates Masks for finding contexts with certain Hamming-distance
+     * @param length length of of context
+     * @param distance Hamming-Distance
+     */
+    private void generate_masks(int length, int distance){
+        Integer[]ones = new Integer[length-distance];
+        Arrays.fill(ones, Integer.MAX_VALUE);
+        Integer []zeros = new Integer[distance];
+        Arrays.fill(zeros, 0);
+        ArrayList<Integer> alphabet = new ArrayList<Integer>();
+        alphabet.addAll(Arrays.asList(ones));
+        alphabet.addAll(Arrays.asList(zeros));
+        permutingArray(alphabet, 0);
+        this.masks = new HashSet<ArrayList<Integer>>(this.permutations);
+
+
+    }
+
+    /**
+     * Used to create all possible masks referring to a set Hamming-Distance
+     * @param arrayList
+     * @param element
+     */
+    private void permutingArray(ArrayList<Integer> arrayList, int element) {
+        for (int i = element; i < arrayList.size(); i++){
+            java.util.Collections.swap(arrayList, i, element);
+            permutingArray(arrayList, element + 1);
+            java.util.Collections.swap(arrayList, element, i);
+        }
+        if (element == arrayList.size() - 1) {
+            ArrayList<Integer> list3 = new ArrayList<Integer>(arrayList);
+            this.permutations.add(list3);
+        }
+    }
+
+    /**
+     * Returns list of indices of the contexts that are similar to a certain context
+     * @param is_similar_to context to compare with
+     * @param contexts list of all possible contexts
+     * @return
+     */
+    private ArrayList<Integer> get_similar(byte[] is_similar_to, List<byte[]> contexts) {
+        ArrayList<Integer> idx = new ArrayList<Integer>();
+        boolean is_similar = false;
+        for(ArrayList<Integer>mask : this.masks){
+            for(int id = 0; id < contexts.size(); id++){
+                byte[] context = contexts.get(id);
+                is_similar = false;
+                for(int i = 0; i < context.length; i++){
+                    if(is_similar){
+                        idx.add(id);
+                        break;
+                    }
+                    is_similar = ((is_similar_to[i] & mask.get(i)) & context[i]) == (is_similar_to[i] & mask.get(i));
+                }
+            }
+        }
+        return idx;
+    }
+
+
+
+    private ArrayList<Pandas> copy(ArrayList<Pandas> src){
+        ArrayList<Pandas> dest = new ArrayList<Pandas>();
+        for( Pandas original : src) {
+            Pandas clone = new Pandas(original);
+            dest.add(clone);
+        }
+        return dest;
+    }
+
+
+
+    //Getter and Setter methods
+    public ArrayList<Pandas> getVMM(){
+        return this.prob_mats;
+    }
+
+    public ArrayList<Pandas> getCounts(){
+        return this.counts;
+    }
+
+    public ArrayList<String> getAlphabet(){
+        return this.alphabet;
+    }
+
+    public int getMax_order(){
+        return this.max_depth;
+    }
+
+    public String getGenerated_history(){
+        return this.generated_history;
+    }
+
+    public String getInput_history() { return this.input_history; }
+
+
+    public static VMM loadVMM(String name) {
+        try {
+            FileInputStream fileIn = new FileInputStream(name);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            VMM vomm = (VMM) in.readObject();
+            in.close();
+            fileIn.close();
+            System.out.println("Loaded Serialized VMM saved in " + name);
+            return vomm;
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("VMM class not found");
+            c.printStackTrace();
+        }
+        throw new RuntimeException("Failed loading");
+    }
+
+    /**
+     * Load VOMM with name vmm.ser in directory
+     */
+    public static VMM loadVMM() {
+
+        try {
+            FileInputStream fileIn = new FileInputStream("vmm.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            VMM vomm = (VMM) in.readObject();
+            in.close();
+            fileIn.close();
+            System.out.println("Loaded Serialized VMM saved in vmm.ser");
+            return vomm;
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("VMM class not found");
+            c.printStackTrace();
+        }
+        throw new RuntimeException("Loading failed");
+    }
+
+
+    /**
+     * Stores VOMM in vmm.ser
+     */
+    public void writeVMM() {
+        try {
+            FileOutputStream fileOut = new FileOutputStream("vmm.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(this);
+            out.close();
+            fileOut.close();
+            System.out.println("Serialized VMM is saved in vmm.ser");
+
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Stores Vomm at set directory
+     *
+     * @param file Directory to save to
+     */
+    public void writeVMM(String file) {
+        try {
+            FileOutputStream fileOut = new FileOutputStream(file);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(this);
+            out.close();
+            fileOut.close();
+            System.out.println("Serialized data is saved in " + file);
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+}
+
+
---
+++ VommJava/vomm/lib/vomm/lib/Pandas.java
@@ -0,0 +1,237 @@
+package lib;
+
+/**
+ * The Pandas program implements the functions I need from the python pandas library
+ *
+ * @author Jonas Kraasch
+ * @since 2018-07-17
+ */
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+
+
+
+public class Pandas implements java.io.Serializable {
+
+    //Order of VOMM
+    int depth;
+    //List of String corresponding to the alphabet used in this order of the VOMM
+    public ArrayList<String> alphabet;
+    //Converting alphabet to their  (needed for Sample method only takes array not collection)
+    public int[] alpha_pos;
+    //Row Index (Context -> Index)
+    HashMap<ArrayList<String>, Integer> index_0;
+    //Row Name (Index -> Context)
+    public HashMap<Integer, ArrayList<String>> inverse_index_0;
+    //Column Index (Alphabet -> Index)
+    public HashMap<String, Integer> index_1;
+    // Binarized Contexts used for bitwise comparison with masks to get contexts with set Hamming-Distance
+    public HashMap<ArrayList<String>, byte[]> binarized = new HashMap();
+    // Inverse of Binarized
+    HashMap<byte[], ArrayList<String>> inverse_binarized = new HashMap();
+
+    //DataFrame
+    public ArrayList<ArrayList<Double>> df = new ArrayList<ArrayList<Double>>();
+
+
+    /**
+     * @param alphabet Array of Strings use in Dataset
+     * @param depth    Order of Vomm
+     */
+    public Pandas(ArrayList<String> alphabet, int depth) {
+
+        index_0 = new HashMap();
+        index_1 = new HashMap();
+        inverse_index_0 = new HashMap();
+        this.depth = depth;
+        this.alphabet = alphabet;
+        this.alpha_pos = new int[alphabet.size()];
+        for (int i = 0; i < alphabet.size(); i++) {
+            this.index_1.put(alphabet.get(i), i);
+            this.alpha_pos[i] = i;
+        }
+    }
+
+    public Pandas(Pandas src) {
+        index_0 = (HashMap<ArrayList<String>, Integer>) src.index_0.clone();
+        index_1 = (HashMap<String, Integer>) src.index_1.clone();
+        inverse_index_0 = (HashMap<Integer, ArrayList<String>>) src.inverse_index_0.clone();
+        for(ArrayList<Double>old_df: src.df){
+            this.df.add((ArrayList<Double>) old_df.clone());
+        }
+        this.depth = src.depth;
+        this.alphabet = (ArrayList<String>) src.alphabet.clone();
+        this.binarized = (HashMap<ArrayList<String>, byte[]>) src.binarized.clone();
+        this.inverse_binarized = (HashMap<byte[], ArrayList<String>>) src.inverse_binarized.clone();
+        this.alpha_pos = src.alpha_pos.clone();
+
+    }
+
+    /**
+     * @return number of contexts in Dataframe
+     */
+    public int length() {
+        return df.size();
+    }
+
+    /**
+     * Returns context which was mapped to index
+     *
+     * @param index Index that you want the context of (Row-Index)
+     * @return
+     */
+    public ArrayList<String> getContext(int index) {
+        return inverse_index_0.get(index);
+    }
+
+    /**
+     * Add String to DataFrame, adds context with its reference in the Dataframe to dictionaries
+     *
+     * @param context String to be added (Row-Name)
+     */
+    public void addContext(ArrayList<String> context) {
+        if (context.size() > depth) {
+            throw new RuntimeException("Context is to long!");
+        }
+        this.index_0.put(context, df.size());
+        byte[] binarized = Helper.binarize(context);
+        this.binarized.put(context, binarized);
+        this.inverse_binarized.put(binarized, context);
+        this.inverse_index_0.put(df.size(), context);
+        ArrayList<Double> context_list = new ArrayList<Double>(Collections.nCopies(this.alphabet.size(), 0.0));
+        df.add(context_list);
+    }
+
+    /**
+     * Sets counts of every symbol appearing after the context to the values in the array values
+     *
+     * @param context String context you want to change the value of (Row-Name)
+     * @param values  Array of values to set
+     */
+    public void setValue(ArrayList<String> context, ArrayList<Double> values) {
+        if (this.index_0.containsKey(context)) {
+            df.set(this.index_0.get(context), values);
+        } else {
+            this.addContext(context);
+            this.setValue(context, values);
+        }
+    }
+
+    /**
+     * Sets count of symbol appearing after context to value
+     *
+     * @param context String context you want to change the value of (Row-Name)
+     * @param symbol  String of symbol you want to change the value of (Column-Name)
+     * @param value   value to set
+     */
+    public void setValue(ArrayList<String> context, String symbol, double value) {
+
+        if (this.index_0.containsKey(context)) {
+            df.get(this.index_0.get(context)).set(this.index_1.get(symbol), value);
+            if (!this.index_1.containsKey(symbol)) {
+                this.index_1.put(symbol, this.index_1.size());
+                int[] new_alpha_pos = new int[this.alpha_pos.length+1];
+                for(int i = 0; i < new_alpha_pos.length; i++){
+                    new_alpha_pos[i] = i;
+                }
+                this.alpha_pos = new_alpha_pos;
+                this.alphabet.add(symbol);
+                df.get(this.index_0.get(context)).add(0.0);
+            }
+        } else {
+            this.addContext(context);
+            this.setValue(context, symbol, value);
+        }
+    }
+
+    /**
+     * Increments count for symbol appearing after context
+     *
+     * @param context String context to increment the occurrence of symbol (Row-Name)
+     * @param symbol  Sting symbol that appeared and the occurrence needs to be incremented of (Column-Name)
+     */
+    public void incrementValue(ArrayList<String > context, String symbol) {
+
+        if (this.index_0.containsKey(context)) {
+            if (!this.index_1.containsKey(symbol)) {
+                //System.out.println("Symbol not in alphabet");
+                this.index_1.put(symbol, this.index_1.size());
+                int[] new_alpha_pos = new int[this.alpha_pos.length+1];
+                for(int i = 0; i < new_alpha_pos.length; i++){
+                    new_alpha_pos[i] = i;
+                }
+                this.alpha_pos = new_alpha_pos;
+                this.alphabet.add(symbol);
+                df.get(this.index_0.get(context)).add(0.0);
+
+            }
+            df.get(this.index_0.get(context)).set(this.index_1.get(symbol), df.get(this.index_0.get(context)).get(this.index_1.get(symbol)) + 1);
+        } else {
+            this.addContext(context);
+            this.incrementValue(context, symbol);
+        }
+    }
+
+    /**
+     * Get array of values stored in position [context]
+     *
+     * @param context String context to get the values from (Row-Name)
+     * @return Array of all the values in the row of context
+     */
+    public ArrayList<Double> getValue(ArrayList<String> context) {
+        if (context.size() == 0) {
+            return df.get(0);
+        }
+        if (!this.index_0.containsKey(context)) {
+            return this.getValue(new ArrayList<String>(context.subList(1,context.size())));
+        }
+        return df.get(this.index_0.get(context));
+    }
+
+    /**
+     * Returns value stored in position [context][symbol]
+     *
+     * @param context String context to get the values from (Row-Name)
+     * @param symbol  String symbol to get the value from (Column-Name)
+     * @return Value of Symbol given Context
+     */
+    public double getValue(ArrayList<String> context, String symbol) {
+        if (!this.index_1.containsKey(symbol)) {
+            throw new RuntimeException("Symbol not in alphabet!");
+        }
+        if (!this.index_0.containsKey(context)) {
+            throw new RuntimeException("Context not in training sequences!");
+        }
+        return df.get(this.index_0.get(context)).get(this.index_1.get(symbol));
+    }
+
+    /**
+     * Print values given row context
+     *
+     * @param context String context to print the vales from (Row-Name)
+     * @return Printable String of values in row context
+     */
+    public String toString(ArrayList<String> context) {
+        if (!this.index_0.containsKey(context)) {
+            throw new RuntimeException("Context not in training sequences!");
+        }
+        String values = "";
+        for (double i : this.getValue(context)) {
+            values += " | " + i;
+        }
+        String alphabet = "";
+        for (String letter : this.alphabet) {
+            alphabet += " | " + letter;
+        }
+
+        return alphabet + "\n" + context + "\n" + values;
+    }
+
+    public int getDepth() {
+        return this.depth;
+    }
+}

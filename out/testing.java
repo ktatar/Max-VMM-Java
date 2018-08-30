118e6c53b6b27959b15d341bda508338ce420e4d
jkraasch <33602014+jkraasch@users.noreply.github.com>
2018-08-30 16:58:47 -0700

    Add files via upload

---
+++ out/testing.java
@@ -0,0 +1,30 @@
+import VMMmeta;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.UnsupportedEncodingException;
+
+public class testing{
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String[] alphabet = {"1", "2", "3", "4", "5", "6"};
+        ArrayList<String> seq = new ArrayList<String>(Arrays.asList(alphabet));
+
+        VMM vmm = new VMM(seq, 2);
+        vmm.learn(seq);
+        System.out.print(vmm.getAlphabet());
+
+        String[] new_alphab = {"7","8","9","10"};
+        ArrayList<String> new_seq = new ArrayList<String>(Arrays.asList(new_alphab));
+        System.out.println("--------------------");
+        vmm.learn(new_seq);
+        System.out.println(vmm.getAlphabet());
+        ArrayList<String> values = new ArrayList<String>(vmm.prob_mats.get(1).index_1.keySet() );
+        System.out.println("2-layer" + vmm.prob_mats.get(1).length());
+        //System.out.println(Arrays.asList(vmm.prob_mats.get(1).index_1));
+        System.out.println(vmm.alphabet);
+
+
+
+    }
+}
